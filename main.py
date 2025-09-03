92ee51faf6281d35b4212e0f671095e528140f9d
FitiavanaLova <hei.fitiavana@gmail.com>
2025-09-03 12:14:25 +0300

    the answaer

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+from fastapi import FastAPI, HTTPException, Header
+from pydantic import BaseModel
+from typing import List
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/ping")
+def ping():
+    return "pong"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class CarsModel(BaseModel):
+    id: str
+    brand: str
+    model: str
+    characteristics: object
+    characteristics = {
+        max_speed: int,
+        max_fuel_capacity: int
+    }
+
+
+cars_store: List[CarsModel] = []
+
+
+@app.post("/cars", status_code=201)
+def create_cars(students: List[CarsModel]):
+    cars_store.extend(students)
+    return JSONResponse(cars_store, status_code=201)
+
+
+@app.get("/cars")
+def get_cars():
+    return cars_store
+
+
+@app.get("/cars-id")
+def get_cars_id(id: str):
+    if not id :
+        return JSONResponse(cars_store, status_code=401)
+    if id:
+        return cars_store
